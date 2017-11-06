9b58f459e28256a36927322809852244bcf6d032
Christopher Milne <christopher.milne@gmail.com>
2017-11-05 21:13:59 -0500

    convert app to use umbrella project structure

--- mix.exs
+++ mix.exs
@@ -1,32 +1,20 @@
-defmodule MetarScraper.Mixfile do
+defmodule NavcanadaMetarApi2.Mixfile do
   use Mix.Project
 
   def project do
     [
-      app: :metar_scraper,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      apps_path: "apps",
       start_permanent: Mix.env == :prod,
       deps: deps()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
-      extra_applications: [:logger, :poolboy],
-      mod: {MetarScraper.Application, []}
-    ]
-  end
-
-  # Run "mix help deps" to learn about dependencies.
+  # Dependencies listed here are available only for this
+  # project and cannot be accessed from applications inside
+  # the apps folder.
+  #
+  # Run "mix help deps" for examples and options.
   defp deps do
-    [
-      {:hound, "~> 1.0"},
-      {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
-      {:poolboy, "~> 1.5"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-    ]
+    []
   end
 end
