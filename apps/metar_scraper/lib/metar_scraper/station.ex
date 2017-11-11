fdab4d9472d7b0756ccca972c6aa8773340d3980
Christopher Milne <christopher.milne@gmail.com>
2017-11-11 15:33:19 -0500

    switch to NOAA data source from navcanada who updated their site to prevent scraping

--- apps/metar_scraper/lib/metar_scraper/station.ex
+++ apps/metar_scraper/lib/metar_scraper/station.ex
@@ -1,9 +1,4 @@
 defmodule MetarScraper.Station do
-  # TODO should this be named __MODULE.Aerodrome or something else ??
-  @enforce_keys [:station, :current, :history, :taf]
-  defstruct station: nil, region: nil, current: nil, history: [], taf: nil
-
-  def fake do
-    %__MODULE__{current: "", history: [], region: :ontario_quebec, station: :CYYZ, taf: ""}
-  end
+  @enforce_keys [:station, :reports]
+  defstruct station: nil, reports: nil, longitude: nil, latitude: nil, flight_category: nil, latest_observation_time: nil, sea_level_pressure_mb: nil, elevation_m: nil
 end
--- apps/metar_scraper/mix.exs
+++ apps/metar_scraper/mix.exs
@@ -14,7 +14,7 @@ defmodule MetarScraper.Mixfile do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :poolboy],
+      extra_applications: [:logger, :poolboy, :httpoison],
       mod: {MetarScraper.Application, []}
     ]
   end
@@ -22,11 +22,10 @@ defmodule MetarScraper.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:hound, "~> 1.0"},
       {:mix_test_watch, "~> 0.3", only: :dev, runtime: false},
-      {:poolboy, "~> 1.5"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:poolboy, "~> 1.5"},
+      {:httpoison, "~> 0.13"},
+      {:sweet_xml, "~> 0.6.5"}
     ]
   end
 end
