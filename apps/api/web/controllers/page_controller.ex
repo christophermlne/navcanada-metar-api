3e86b5262ffd37bab0489695b2ac84f866974227
Christopher Milne <christopher.milne@gmail.com>
2017-11-23 14:21:24 -0500

    whitespace adjustment

--- apps/api/web/controllers/page_controller.ex
+++ apps/api/web/controllers/page_controller.ex
@@ -5,12 +5,7 @@ defmodule Api.PageController do
     import MetarService
 
     case get(station_id) do
-      {:ok, %{data: %{taf: taf,
-                      metar: %{ reports: reports }
-                     },
-                     retrieved_at: retrieved_at
-             }
-      } ->
+      {:ok, %{data: %{taf: taf, metar: %{reports: reports}}, retrieved_at: retrieved_at}} ->
         render conn, "index.html", station_id: station_id, report: reports, taf: taf, error: nil, retrieved_at: retrieved_at
       {:error, msg} ->
         render conn, "index.html", station_id: station_id, report: %{ history: [], currrent: ""}, taf: "", error: msg, retrieved_at: nil
